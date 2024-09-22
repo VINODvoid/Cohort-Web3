928ab6fb98fe8c667a14c32cf9e033e9edaa3cc1
VINODvoid <iamlearner.v@gmail.com>
2024-09-22 23:44:09 +0530

    Add .gitignore and assignment01 files

---
+++ Week-01/assigments/assignment01/index.js
@@ -0,0 +1,22 @@
+const crypto = require('crypto');
+
+function getHash(prefix){
+    let input = 0;
+    while (true){
+        let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if(hash.startsWith(prefix))
+        {
+            return {
+                input:inputStr,
+                hash:hash,
+            }
+        }
+        input++;
+    }
+
+}
+const value = getHash('00000')
+console.log(value.hash);
+console.log(value.input);
+
