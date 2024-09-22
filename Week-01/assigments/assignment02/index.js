1e1f99037288546966443cd14c26b3911a324bf7
VINODvoid <iamlearner.v@gmail.com>
2024-09-23 00:00:50 +0530

    feat : completed assignment 2

---
+++ Week-01/assigments/assignment02/index.js
@@ -0,0 +1,25 @@
+// Give me an input string that outputs a SHA-256 hash that starts with 00000 
+// What if I ask you that the input string should start with 100xdevs ? How would the code change?
+
+const crypto = require('crypto');
+
+function customHashWithPrefix(prefix){
+    let input = 0;
+    while(true){
+        let inputStr= "100xdevs"+input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if(hash.startsWith(prefix))
+        {
+            return {
+                input:inputStr,
+                hash : hash,
+            }
+        }
+        input++;
+    }
+}
+
+
+const value = customHashWithPrefix('00000');
+console.log(value.hash);
+console.log(value.input);
