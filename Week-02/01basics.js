05a9df65b89b0b5a0b9bfa92826342d11d73ac20
VINODvoid <iamlearner.v@gmail.com>
2025-03-20 20:31:51 +0530

    feat: Add basic JavaScript file with variable declarations and console logs

---
+++ Week-02/01basics.js
@@ -0,0 +1,21 @@
+const a  = 1;
+console.log(a);
+
+
+const b= true;
+console.log(b);
+
+const x = 202
+console.log(x);
+
+const bytes = [202, 244, 1, 23]
+console.log(bytes);
+
+
+let byte = new Uint8Array([0, 255, 127, 128]);
+console.log(bytes)
+
+// assignment 
+let uint8Arr = new Uint8Array([0, 255, 127, 128]);
+uint8Arr[1] = 300;
+console.log(uint8Arr);
