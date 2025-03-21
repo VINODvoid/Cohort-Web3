fbfa6b909f1a5bc4b45025a10e6b162df109b343
VINODvoid <iamlearner.v@gmail.com>
2025-03-21 08:10:28 +0530

    feat: Add functions to convert bytes to hex and hex to bytes in bitconversion.js

--- Week-02/bitconversion.js
+++ Week-02/bitconversion.js
@@ -30,7 +30,7 @@ function bytesToAscii(byteArray) {
     return new TextDecoder().decode(new Uint8Array(byteArray));
   }
   
-  // Example usage:
+
   const byteArray = new Uint8Array([72, 101, 108, 108, 111]); 
   const asciiCharacters = bytesToAscii(bytes);
   console.log(asciiString);
@@ -42,8 +42,36 @@ function bytesToAscii(byteArray) {
     return new Uint8Array([...asciiString].map(char => char.charCodeAt(0)));
   }
   
-  // Example usage:
+ 
   const asciicharacter  = "hello";
   const byte = asciiToBytes(asciicharacter);
   console.log(byte); 
+  
+//   bytes to hexa
+  function arrayToHex(byteArray) {
+    let hexString = '';
+    for (let i = 0; i < byteArray.length; i++) {
+      hexString += byteArray[i].toString(16).padStart(2, '0');
+    }
+    return hexString;
+  }
+  
+ 
+  const byteArray1 = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+  const hexString = arrayToHex(byteArray1);
+  console.log(hexString); // Output: "48656c6c6f"
+  
+//   hexa to bytes
+function hexToArray(hexString) {
+    const byteArray = new Uint8Array(hexString.length / 2);
+    for (let i = 0; i < byteArray.length; i++) {
+      byteArray[i] = parseInt(hexString.substr(i * 2, 2), 16);
+    }
+    return byteArray;
+  }
+  
+
+  const hex = "48656c6c6f";
+  const byteArrayFromHex = hexToArray(hex);
+  console.log(byteArrayFromHex); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
   
