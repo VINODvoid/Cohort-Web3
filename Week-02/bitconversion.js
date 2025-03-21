970d682429d644f253011a84987841b9cc3fff26
VINODvoid <iamlearner.v@gmail.com>
2025-03-21 06:02:52 +0530

    feat: Add function to convert ASCII to bytes in bitconversion.js

--- Week-02/bitconversion.js
+++ Week-02/bitconversion.js
@@ -10,3 +10,17 @@ function convertToAscii(bytes)
 const bytes = [104 ,101 ,108 ,108 ,111]
 const asciiString = convertToAscii(bytes);
 console.log(asciiString);
+
+// ascii to bytes
+function convertToByte(ascii)
+{
+    let byteArray = [];
+    for(let i=0;i<ascii.length;i++)
+    {
+        byteArray.push(ascii.charCodeAt(i));
+    }
+    return byteArray;
+}
+const ascii = "hello";
+const bytecode = convertToByte(ascii);
+console.log(bytecode);
