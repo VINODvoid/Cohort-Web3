80b7f7b6640f7cde535b14800c9962df7f0ca3eb
VINODvoid <iamlearner.v@gmail.com>
2025-03-23 09:58:33 +0530

    feat: Enhance mnemonic functionality by adding seed generation in Mnemonics.js

--- Week-02/Mnemonics.js
+++ Week-02/Mnemonics.js
@@ -1,4 +1,7 @@
-import {generateMnemonic} from 'bip39'
+import {generateMnemonic,mnemonicToSeedSync} from 'bip39'
 
 const words = generateMnemonic(128);
 console.log(words);
+const seed = mnemonicToSeedSync(words);
+console.log(seed);
+
