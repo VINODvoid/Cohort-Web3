d082e660306067214dc0d98613c32a9a832a6c40
VINODvoid <iamlearner.v@gmail.com>
2025-03-26 22:23:21 +0530

    feat: Add function stub for deriving Ethereum wallet from seed

---
+++ Week-03/index.js
@@ -0,0 +1,8 @@
+
+// export function deriveEthereumWallet(
+//     seed: Buffer,
+//     derivationPath: string
+//   ): Wallet {
+//     const privateKey = deriveEthereumPrivateKey(seed, derivationPath);
+//     return new Wallet(privateKey);
+//   }
