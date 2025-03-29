5a01ffeaa8bca0ee34326173041c23eb6448a8d0
VinodKALKI <122192120+VINODvoid@users.noreply.github.com>
2025-03-29 22:49:34 +0530

    Update index.js

--- Week-03/index.js
+++ Week-03/index.js
@@ -7,5 +7,6 @@
 //     return new Wallet(privateKey);
 //   }
 // important tip on web3 
+
 // Always ensure that your private keys are securely stored and never exposed in your codebase.
 // Use environment variables or secure vaults to manage sensitive information.
